628c6d93cc0a4abda45a77c4aed78cecade38467
satvikmehan <satvikmehan@gmail.com>
2025-12-08 00:04:57 +0530

    new

--- run.sh
+++ run.sh
@@ -15,4 +15,4 @@ pip install --upgrade pip
 pip install -r requirements.txt
 
 # Start the server on port 8000
-python -m uvicorn main:app --host 0.0.0.0 --port 8000
+python3 -m uvicorn main:app --host 0.0.0.0 --port 8000
